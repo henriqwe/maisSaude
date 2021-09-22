57678ff31bc6d18b772e318bcea2297ac5c6131d
Henriqwe <contato@henriqwedev.com.br>
2021-09-22 19:30:53 -0300

    instalaçao das dependenicias e estrutura de rotas

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #PROJETO TO-DO (FRONT) 
+
+- FontAwesome (Icones) -- NÃO INSTALADO
+- React Router Dom (Roteamento) ok
+- React BootStrap (CSS) ok
+- React-Toastify (Toast) ok
+- Axios (Conexão com API) ok
+- Date-fns (Formatação de datas) ok
--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>teste</h1>
-      </header>
-    </div>
+    <>
+      <Routes/>
+    </>
   );
 }
 
