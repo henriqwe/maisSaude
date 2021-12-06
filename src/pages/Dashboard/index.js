0e0d5f15ee9a024c17350892413b0f4abdb93ae0
adonis <adonisinoda07@gmail.com>
2021-12-06 07:44:51 -0300

    mudanças a serem descartadas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import "./Dashboard.css"
 
 export function Dashboard(){
     return(
-        <div>Dashboard</div>
+        <div className="box">
+        <div className="header">
+            <h1 >To-do</h1>
+            <select >
+            <option value="0">Concluído</option>
+            <option value="1">Em Andamento</option>
+            <option value="2">Não Inicializada</option>
+            </select>
+            <button>Filtrar</button>
+
+
+           
+            <ul className="lista">
+
+                <li>TITULO DO TO-DOLIST-1 <button className="btn-visu">Visualizar</button> <button className="btn-editar">Editar</button><button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-1 <button className="btn-visu">Visualizar</button> <button className="btn-editar">Editar</button><button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-1 <button className="btn-visu">Visualizar</button> <button className="btn-editar">Editar</button><button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-1 <button className="btn-visu">Visualizar</button> <button className="btn-editar">Editar</button><button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-1 <button className="btn-visu">Visualizar</button> <button className="btn-editar">Editar</button><button className="btn-excluir">excluir</button></li> 
+
+                <li>TITULO DO TO-DOLIST-2 <button className="btn-visu">Visualizar</button> <button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-2 <button className="btn-visu">Visualizar</button> <button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-2 <button className="btn-visu">Visualizar</button> <button className="btn-excluir">excluir</button></li> 
+                <li>TITULO DO TO-DOLIST-2 <button className="btn-visu">Visualizar</button> <button className="btn-excluir">excluir</button></li> 
+
+                
+
+            </ul>
+
+           
+        </div>
+        </div>
+        
     )
 }
