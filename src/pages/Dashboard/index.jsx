c71f954e3a7cf1df45e2724d86559e814edb49b4
Henriqwe <contato@henriqwedev.com.br>
2021-12-06 11:50:40 -0300

    filter

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,24 +1,29 @@
 import React, { useState, useEffect, useContext } from "react";
 import Header from "../../components/Header/Header";
 import { UserContext } from "../../context/UserContext";
-import { ToastContainer,Slide, toast } from "react-toastify";
+import { ToastContainer, Slide, toast } from "react-toastify";
 import api from "../../service/api";
 import NewTask from "../../components/NewTask";
 import { DropdownButton, Dropdown, Container, Row, Col } from "react-bootstrap";
 import CardTask from "../../components/CardTask";
 
 export function Dashboard() {
-
   const [tasks, setTasks] = useState([]);
   const { user } = useContext(UserContext);
-  // const [tasksFiltred, setTasksFiltred] = useState([]);
+  const [tasksFiltred, setTasksFiltred] = useState([]);
 
   useEffect(async () => {
     await api.get(`api/getTasksUser/${user.id}`).then((res) => {
       setTasks(res.data);
-      // setTasksFiltred(res.data)
+      setTasksFiltred(res.data);
     });
   }, [user.id]);
+
+  useEffect(() => {
+    setTasksFiltred(tasks);
+  }, [tasks]);
+
+ 
 
   return (
     <>
@@ -26,22 +31,48 @@ export function Dashboard() {
       <Container className='px-5 mt-4'>
         <Row>
           <Col>
-            <NewTask setTasks={setTasks} value={{ToastContainer, Slide, toast}} />
+            <NewTask
+              setTasks={setTasks}
+              value={{ ToastContainer, Slide, toast }}
+            />
           </Col>
           <Col className='justify-content-end d-flex'>
-            <FilterBtn />
+            <FilterBtn  tasks={tasks} setTasksFiltred={setTasksFiltred}/>
           </Col>
         </Row>
         <Row>
           <Col className='justify-content-center d-flex'>
             <ul className='p-0'>
-              {tasks.map(({id,titulo,descricao,prioridade_tarefa,categoria,data_e_hora_tarefa,status}) => {
-                return (
-                  <li key={id} className='text-white list-unstyled'>
-                    <CardTask value={{id,titulo,descricao,prioridade_tarefa,categoria,data_e_hora_tarefa,status,setTasks,ToastContainer, Slide, toast }} />
-                  </li>
-                );
-              })}
+              {tasksFiltred.map(
+                ({
+                  id,
+                  titulo,
+                  descricao,
+                  prioridade_tarefa,
+                  categoria,
+                  data_e_hora_tarefa,
+                  status,
+                }) => {
+                  return (
+                    <li key={id} className='text-white list-unstyled'>
+                      <CardTask
+                        value={{
+                          id,
+                          titulo,
+                          descricao,
+                          prioridade_tarefa,
+                          categoria,
+                          data_e_hora_tarefa,
+                          status,
+                          setTasks,
+                          Slide,
+                          toast,
+                        }}
+                      />
+                    </li>
+                  );
+                }
+              )}
             </ul>
           </Col>
         </Row>
@@ -51,13 +82,27 @@ export function Dashboard() {
   );
 }
 
-function FilterBtn() {
+function FilterBtn({tasks,setTasksFiltred}) {
+
+  function filterTasks(status) {
+    console.log(status);
+    if(status === 3){
+      setTasksFiltred(tasks)
+    }else{
+      let filter = tasks.filter(task=> (task.status === status));
+      setTasksFiltred(filter)
+    }
+  }
   return (
     <DropdownButton align='end' title='Filtro'>
-      <Dropdown.Item eventKey='1'>todos</Dropdown.Item>
-      <Dropdown.Item eventKey='2'>Não inciados</Dropdown.Item>
-      <Dropdown.Item eventKey='3'>Em andamento</Dropdown.Item>
-      <Dropdown.Item eventKey='4'>Concluidos</Dropdown.Item>
+      <Dropdown.Item onClick={() => filterTasks(3)}>todos</Dropdown.Item>
+      <Dropdown.Item onClick={() => filterTasks(0)}>
+        Não inciados
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => filterTasks(1)}>
+        Em andamento
+      </Dropdown.Item>
+      <Dropdown.Item onClick={() => filterTasks(2)}>Concluidos</Dropdown.Item>
     </DropdownButton>
   );
 }
