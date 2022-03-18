c5afa2dfeea0ad6c3f7f52a1661a335ff0c3bb73
AdonisinodA <adonisinoda@gmail.com>
2022-03-18 17:38:11 -0300

    imc

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import { BrowserRouter, Switch } from "react-router-dom";
-import { Login } from "../pages/Login";
-import { Register } from "../pages/Register";
-import { Dashboard } from "../pages/Dashboard";
-import {Imc} from "../pages/Imc"
-import { PageNotFound } from "../pages/PageNotFound"
-import RouterWapper from "./Router";
+import { BrowserRouter, Switch } from 'react-router-dom'
+import { Login } from '../pages/Login'
+import { Register } from '../pages/Register'
+import { Dashboard } from '../pages/Dashboard'
+import { Imc } from '../pages/Imc'
+import { PageNotFound } from '../pages/PageNotFound'
+import RouterWapper from './Router'
 
 const Routes = () => {
-
   return (
     <BrowserRouter>
       <Switch>
         <RouterWapper exact path="/" component={Login} />
         <RouterWapper exact path="/register" component={Register} />
-        <RouterWapper exact isPrivate={true} path="/dashboard" component={Dashboard} />
-        <RouterWapper exact isPrivate={true} path="/imc" component={Imc} />
+        <RouterWapper
+          exact
+          isPrivate={true}
+          path="/dashboard"
+          component={Dashboard}
+        />
+        <RouterWapper exact path="/imc" component={Imc} />
         <RouterWapper exact path="*" component={PageNotFound} />
       </Switch>
     </BrowserRouter>
-  );
-};
-export default Routes;
+  )
+}
+export default Routes
